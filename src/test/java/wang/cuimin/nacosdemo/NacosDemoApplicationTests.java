2e9ee5c71dc73d6f8dec0cf55bd6d3f584530af6
王翠敏 <pm.wangcuimin@outlook.com>
2023-02-07 20:41:01 +0800

    feat: 初始化 maven 项目

---
+++ src/test/java/wang/cuimin/nacosdemo/NacosDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package wang.cuimin.nacosdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
